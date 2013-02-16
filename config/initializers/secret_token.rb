ab380d1442fb10ef03c51b50f7eed7fa647ab4b0
Bruno Tagliani <shreddingmyheartout@gmail.com>
2013-02-16 20:10:50 -0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sayhiforme::Application.config.secret_token = '80e3277da76df50d032dd015b42a3614239eb1c69a2c93dc5a86a914b2c802fe5788c4ad2823e40589cb646c98d27bbffb3e91060c88d1485a4d8867ccfa1282'
