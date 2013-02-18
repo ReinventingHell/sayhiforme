952639e51a0b5e55b10b5b5de1030a788ce28344
Bruno Tagliani <shreddingmyheartout@gmail.com>
2013-02-18 02:56:26 -0200

    adding token storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
       if auth['info']
          user.name = auth['info']['name'] || ""
          user.email = auth['info']['email'] || ""
+         user.token = auth['credentials']['token']
+         user.token_expiration = auth['credentials']['expires_at']
       end
     end
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'],
+           :scope => "email,publish_stream,friends_photos,friends_birthday"
 end
